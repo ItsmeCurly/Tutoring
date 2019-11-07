310a0c818bd021076ba3d1bd26c3222638ba4b57
Adam <adamg765@gmail.com>
2019-11-07 16:21:08 -0500

    semi working triangle code

--- cos125/triangle.py
+++ cos125/triangle.py
@@ -1,9 +1,23 @@
 def triangle(width, height):
     for i in range(height):
-        spacing = int((height-i)/2)
-        space_str = spacing * "-"
-        inner_space_str = (width - spacing*2 - 2) * "-"
-        print(f"{space_str}*{inner_space_str}*{space_str}")
-    
+        for j in range(width):
+            char = computeChar(height, width, i, j)
+            print(char, end = '')
+        print()
+   
+def computeChar(height, width, i, j):
+    if (i == 0 or i == height -1)and j == int(width/2):
+        return 'o'
+    elif j == int(width/2):
+        return 'e'
+    elif i == height - 1:
+        return 'o'
+    elif j == int(width/2 + (width/height) * ((i+1)/2))-1 or j == int(width/2 - (width/height) * ((i+1)/2))+1:
+        return 'o'
+    elif j < int(width/2 + (width/height) * ((i+1)/2)) and j > int(width/2 - (width/height) * ((i+1)/2)):
+        return 'e'
+    else:
+        return ' '
+     
 if __name__ == "__main__":
-    triangle(5, 19)
+    triangle(23, 1)
